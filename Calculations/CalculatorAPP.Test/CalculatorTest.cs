b1fc294da26b0437330b1a292f8114ceeedbbda7
Danielle Amaral Constancio e Silva <danielle.constancio@ambevtech.com.br>
2022-04-08 18:10:02 -0300

    inicia estudo sobre xunit

---
+++ Calculations/CalculatorAPP.Test/CalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace CalculatorAPP.Test
+{
+    public class CalculatorTest
+    {
+        // NomeDaFuncao__EntradaDeValores__ResultadoEsperado
+        [Fact]
+        public void Add_GivenTwoIntValues_ReturnsInt()
+        {
+            var numeber1 = 2;
+            var number2 = 2;
+
+            var calculator = new Calculator();
+            var result = calculator.Add(numeber1, number2);
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Add_GivenTwoDoubleValues_ReturnsDouble()
+        {
+            var double1 = 2.2;
+            var double2 = 2.1;
+
+            var calculator = new Calculator();
+            var result = calculator.AddDouble(double1, double2);
+
+            Assert.Equal(4.3, result, 1);
+        }
+    }
+}
