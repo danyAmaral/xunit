c42876f669f72e464fcbfcc99ab7250a2c3c2aab
danyAmaral <danyamaral27@gmail.com>
2022-04-22 18:28:22 -0300

    terminado seção de asserts.

--- Calculations/CalculatorAPP.Test/CustomerTest.cs
+++ Calculations/CalculatorAPP.Test/CustomerTest.cs
@@ -1,10 +1,12 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace CalculatorAPP.Test
 {
     public class CustomerTest
     {
         [Fact]
+        [Trait("Category","Rules")]
         public void CheckNameNorEmpty()
         {
             var customer = new Customer();
@@ -21,6 +23,35 @@ namespace CalculatorAPP.Test
 
             Assert.InRange<int>(customer.Age, 25 , 40);
    
+        }
+
+        [Fact]
+        [Trait("Category", "Rules")]
+        public void GetOrdersByNameNotNull()
+        {
+            var customer = new Customer();
+            Assert.Throws<ArgumentException>(() => customer.GetOrdersByName(""));
+
+        }
+
+        [Fact]
+        [Trait("Category", "Rules")]
+        public void GetOrdersByNameNotNullAndCheckMessage()
+        {
+            var customer = new Customer();
+            var exeptionDetails = Assert.Throws<ArgumentException>(() => customer.GetOrdersByName(""));
+
+            Assert.Equal("Name can't be nulll!", exeptionDetails.Message);
+        }
+
+
+        [Fact]
+        public void LoyalCustomerForOrdersG100()
+        {
+            var customer = CustomerFactory.CreateCustomerInstance(105);
+            var loyalCustomer = Assert.IsType<LoyalCustomer>(customer);
+
+            Assert.Equal(10, loyalCustomer.Discount);
         }
     }
 }
--- Calculations/ConsoleApp1/Customer.cs
+++ Calculations/ConsoleApp1/Customer.cs
@@ -1,8 +1,48 @@
-﻿namespace CalculatorAPP
+﻿using System;
+
+namespace CalculatorAPP
 {
     public class Customer
     {
         public string Name = "Maria";
         public int Age = 35;
+
+        public virtual int GetOrdersByName(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name can't be nulll!");
+            }
+            return 100;
+        }
     }
+
+
+    public class LoyalCustomer : Customer {
+
+        public int Discount { get;set; }
+
+        public LoyalCustomer()
+        {
+            Discount = 10;
+        }
+
+        public override int GetOrdersByName(string name)
+        {
+            return 101;
+        }
+    }
+
+    public static class CustomerFactory { 
+    
+        public static Customer CreateCustomerInstance(int orderCount)
+        {
+            if (orderCount > 100)
+                return new LoyalCustomer();
+
+            return new Customer();
+        }
+    }
+
+
 }
